fdde5ef659fd015f79e6e4f25da60451df180a7f
gaurav1209 <aggarwal.gaurav10@gmail.com>
2019-03-27 15:46:20 +0530

    Inital commit React

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Smart Brain</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
