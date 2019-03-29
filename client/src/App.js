cb194538faf0ded8f5b51ff14b3fbb4eac2d75fa
gaurav1209 <aggarwal.gaurav10@gmail.com>
2019-03-29 21:36:50 +0530

    Added Clarifai and mobile responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
-import ImageForm from './components/InputForm/InputForm';
+import InputForm from './components/InputForm/InputForm';
 import Rank from './components/Rank/Rank';
 
+const app = new Clarifai.App({
+  apiKey: 'd7fa32f8d6064906bf4c9f77865da330'
+});
+
+const ParticlesOptions = {
+  particles: {
+    number:{
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+
 class App extends Component {
+  state = {
+    input: ''
+  };
+
+  onInputChange = event => {
+    this.setState({input: event.target.value});
+  }
+
+  onSubmit = event => {
+    app.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+      .then(generalModel => {
+        return generalModel.predict("https://samples.clarifai.com/metro-north.jpg");
+      })
+      .then(response => {
+        console.log(response);
+      });
+    
+    app.models.predict("d7fa32f8d6064906bf4c9f77865da330", "https://samples.clarifai.com/face-det.jpg")
+    .then(res => {
+      console.log(res);
+    })
+    .catch();
+  }
+
   render() {
     return (
       <div className="App">
+        <Particles className='particles' params={ParticlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageForm />
+        <InputForm onInputChange={this.onInputChange} onSubmitBtn={this.onSubmit}/>
       </div>
     );
   }
--- client/src/components/InputForm/InputForm.js
+++ client/src/components/InputForm/InputForm.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 import './InputForm.css';
 
-const ImageForm = () => {
+const InputForm = ({onInputChange, onSubmitBtn}) => {
     return (
         <div className='container'>
             <p className='f4'>This wil detect faces in the Image.</p>
             <div classNam='center'>
-                <div className='center pa4 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='text' placeholder='Enter image URL'/>
-                    <button className='w-20 grow f4 link ph3 pv2 dib white bg-light-purple pointer'>Detect</button>
+                <div className='res center pa4 shadow-5'>
+                    <input className='f4 pa2 w-70 center' type='text' placeholder='Enter image URL' onChange={onInputChange}/>
+                    <button className='w-20 grow f4 link ph3 pv2 dib white bg-light-purple pointer' onClick={onSubmitBtn}>Detect</button>
                 </div>
             </div>
         </div>
     );
 };
 
-export default ImageForm;
+export default InputForm;
