3d652d9192002ccb373f661c5debe394ca1841ae
gaurav1209 <aggarwal.gaurav10@gmail.com>
2019-03-31 19:52:32 +0530

    Styling Tweaked

--- client/src/components/InputForm/InputForm.js
+++ client/src/components/InputForm/InputForm.js
@@ -6,11 +6,9 @@ const InputForm = ({onInputChange, onSubmitBtn}) => {
     return (
         <div className='container'>
             <p className='f4'>This wil detect faces in the Image.</p>
-            <div classNam='center'>
-                <div className='res center pa4 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='text' placeholder='Enter image URL' onChange={onInputChange}/>
-                    <button className='w-20 grow f4 link ph3 pv2 dib white bg-light-purple pointer' onClick={onSubmitBtn}>Detect</button>
-                </div>
+            <div className='res center pa4 shadow-5'>
+                <input className='f4 pa2 w-70 center' type='text' placeholder='Enter image URL' onChange={onInputChange}/>
+                <button className='w-20 grow f4 link ph3 pv2 dib white bg-light-purple pointer' onClick={onSubmitBtn}>Detect</button>
             </div>
         </div>
     );
